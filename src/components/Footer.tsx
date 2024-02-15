8fc894be9172b9caae562da7ec24248297f05f17
Illuminaxx <nexgen62@hotmail.fr>
2024-02-15 16:02:26 +0100

    🔥 Remove comments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,29 +4,24 @@ const Footer = () => {
   return (
     <div className=" text-white py-6 mt-80">
       <div className="container grid grid-cols-3">
-        {/* Top Row */}
         <div className="flex flex-col ml-32">
-          {/* Section Contact */}
           <div className='mr-24'>
             <h5 className="text-24 font-medium mb-4">Contact</h5>
             <p className="text-18 font-light font-monserrat leading-21.94 tracking-one w-462">Work inquiries: work@vaultflow.com</p>
             <p className="text-18 font-light font-monserrat leading-21.94 tracking-one w-462">PR and speaking: press@vaultflow.com</p>
             <p className="text-18 font-light font-monserrat leading-21.94 tracking-one w-462">New business: newbusiness@vaultflow.com</p>
           </div>
-          {/* Section Careers */}
           <div className="mt-8 text-24 font-medium mb-4">
             <h5 className="text-xl font-semibold mb-3">Careers</h5>
             <p className="text-18 font-light font-monserrat leading-21.94 tracking-one">Careers@vaultflow.com</p>
           </div>
         </div>
         <div className="flex flex-col ml-32">
-          {/* Section Address */}
           <div className="text-24 font-medium mb-4">
             <h5 className="text-xl font-semibold mb-3">Address</h5>
             <p className="text-18 font-light font-monserrat leading-21.94 tracking-one">398 11th Street, Floor 2</p>
             <p className="text-18 font-light font-monserrat leading-21.94 tracking-one">San Francisco, CA 94103</p>
           </div>
-          {/* Section Social */}
           <div className="mt-8 text-24 font-medium mb-4">
             <h5 className="text-xl font-semibold mb-3">Social</h5>
             <p className="text-18 font-light font-monserrat leading-21.94 tracking-one">Twitter</p>
@@ -35,7 +30,6 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      {/* Bottom Row for copyright and logo */}
       <div className="mt-8 pt-4 text-left flex justify-between items-end">
         <p className="text-gray font-montserrat font-light font-16 leading-19.5 tracking-one ml-32">© 2023 Vaultflow. All Rights Reserved.</p>
         <img src={logo} alt="Vaultflow logo" className="h-6 mt-4 mr-12" />
